4a0fd7337feda17f63dbeb74d4a638de8562fa47
Eñaut AA <enaut.irakasle@gmail.com>
2025-01-10 12:49:28 +0100

    dfhdf

---
+++ database/migrations/2025_01_10_114703_create_medicos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre' ,255);
+            $table->string('apellido' ,255);
+            $table->string('fecha_incorporacion' ,255);
+            $table->foreignId('viaje_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medicos');
+    }
+};
